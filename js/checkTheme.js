e68888a87a5c7c162287dae9406bcd918b7b4a47
blecbang <70779434+blecbang@users.noreply.github.com>
2020-09-08 19:56:55 -0700

    Update checkTheme.js

--- js/checkTheme.js
+++ js/checkTheme.js
@@ -5,13 +5,13 @@ function checkTheme() {
 	var buttontext = document.getElementById("button");
 		if (activemode.match("light")) {
 			cs.href = "style/lightmode.css";
-			buttontext.value = "dark mode"
+			buttontext.value = "dark mode";
 		} else if (activemode.match("dark")) {
 			cs.href = "style/darkmode.css";
-			buttontext.value = "light mode"
+			buttontext.value = "light mode";
 		} else {
 			cs.href = "style/lightmode.css";
-			buttontext.value = "dark mode"
+			buttontext.value = "dark mode";
 		}
 }
 
